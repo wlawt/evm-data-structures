9bbfd77a2ebff98e9fa800540f639d55a5a42a38
William Law <williamlaw.wtl@gmail.com>
2022-10-16 22:34:38 -0400

    add: deleteMax test

--- test/PriorityQueues/Heap.t.sol
+++ test/PriorityQueues/Heap.t.sol
@@ -38,4 +38,27 @@ contract HeapTest is Test {
         heap.insert(10);
         assertEq(heap.findMax(), 50);
     }
+
+    function testDeleteMax() public {
+        heap.insert(50);
+        heap.insert(10);
+        heap.insert(29);
+        heap.insert(26);
+        heap.insert(27);
+        heap.insert(15);
+        heap.insert(8);
+        heap.insert(34);
+        heap.insert(23);
+
+        heap.deleteMax();
+
+        assertEq(heap.arr(0), 34);
+        assertEq(heap.arr(1), 27);
+        assertEq(heap.arr(2), 29);
+        assertEq(heap.arr(3), 23);
+        assertEq(heap.arr(4), 26);
+        assertEq(heap.arr(5), 15);
+        assertEq(heap.arr(6), 8);
+        assertEq(heap.arr(7), 10);
+    }
 }
