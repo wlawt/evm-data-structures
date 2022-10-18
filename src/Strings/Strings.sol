15aa5ee46a68061c35d2a946764bf47d43db5a00
William Law <williamlaw.wtl@gmail.com>
2022-10-18 10:36:16 -0400

    add: Brute force patternMatching

--- src/Strings/Strings.sol
+++ src/Strings/Strings.sol
@@ -14,4 +14,29 @@ library Strings {
         }
         return 0;
     }
+
+    // brute force approach
+    function patternMatching(string memory t, string memory p)
+        public
+        pure
+        returns (bool)
+    {
+        uint256 n = bytes(t).length;
+        uint256 m = bytes(p).length;
+
+        for (uint256 i = 0; i < n - m; i++) {
+            bytes memory tmp;
+            uint256 counter = 0;
+            for (uint256 j = i; j < i + m - 1; j++) {
+                tmp[counter] = bytes(t)[j];
+                counter++;
+            }
+            string memory stmp = string(tmp);
+            if (strcmp(i, stmp, p) == 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
